db2c528202380375ff5189177a31ee9c191fb1bb
Haochen Pan <phchcc@gmail.com>
2023-10-11 23:23:04 -0500

    Update setup.py to 0.0.2

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='diaspora_logger',
-    version='0.0.1',
+    version='0.0.2',
     description='A Python logging library for sending logs to Diaspora Streams (Kafka)',
     long_description=long_description,
     long_description_content_type='text/markdown',
