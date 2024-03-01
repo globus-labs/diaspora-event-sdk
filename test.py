11c0847e68e753c9ee942ce5638253e955b63970
Haochen Pan <haochenpan@uchicago.edu>
2024-03-01 10:18:21 -0500

    remove test.py

---
+++ test.py
@@ -0,0 +1,11 @@
+from diaspora_event_sdk import Client as GlobusClient
+c = GlobusClient()
+# topic = "topic100"
+# print(c.register_topic(topic))
+# print(c.list_topics())
+
+# print(c.register_function(topic, "lambda100"))
+
+# print(c.list_functions())
+# print(c.unregister_function(topic, "lambda100"))
+
