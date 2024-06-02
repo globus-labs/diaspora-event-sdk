f0d75ad283d7d02bb3c9e6b4a60639fd8c0eb729
Haochen Pan <haochenpan@uchicago.edu>
2024-06-02 08:46:23 -0400

    pytest api

--- tests/unit/test_apis.py
+++ tests/unit/test_apis.py
@@ -4,6 +4,10 @@ import logging
 from globus_sdk import ConfidentialAppAuthClient
 from diaspora_event_sdk import Client
 from diaspora_event_sdk.sdk.login_manager import tokenstore
+from diaspora_event_sdk.sdk._environments import get_web_service_url
+
+
+# os.environ['DIASPORA_SDK_ENVIRONMENT'] = 'local'
 
 # Configure module-level logger
 logging.basicConfig(
@@ -18,6 +22,7 @@ def setup():
     client_secret = os.environ["DIASPORA_SDK_CLIENT_SECRET"]
     requested_scopes = os.environ["CLIENT_SCOPE"]
 
+
     ca = ConfidentialAppAuthClient(
         client_id=client_id,
         client_secret=client_secret,
@@ -31,6 +36,7 @@ def setup():
     storage.store(token_response)
     storage.get_by_resource_server()
 
+    logger.info(get_web_service_url())
     logger.info(f"Client ID: {client_id}")
     logger.info(f"Client Secret: {client_secret}")
     logger.info(f"Requested Scopes: {requested_scopes}")
@@ -155,6 +161,116 @@ def test_update_topic_configs(setup, client):
 
     # Print the response (optional)
     print(update_response)
+
+
+def test_update_topic_partitions(setup, client):
+    # Generate a unique topic name
+    topic = "topic" + client.subject_openid[-12:]
+
+    # Ensure the topic is registered
+    client.register_topic(topic)
+
+    # Increase the number of partitions
+    new_partitions = 2
+    partitions_response = client.update_topic_partitions(topic, new_partitions)
+
+    # Log the partitions response
+    logger.info(f"Partitions Response: {partitions_response}")
+
+    # Assert the expected outcome
+    assert partitions_response["status"] in [
+        "success",
+        "error",
+    ], "The status should be either success or error"
+    if partitions_response["status"] == "error":
+        assert (
+            "message" in partitions_response
+        ), "The error response should contain a message"
+
+    # Print the response (optional)
+    print(partitions_response)
+
+
+def test_reset_topic(setup, client):
+    # Generate a unique topic name
+    topic = "topic" + client.subject_openid[-12:]
+
+    # Ensure the topic is registered
+    client.register_topic(topic)
+
+    # Reset the topic
+    reset_response = client.reset_topic(topic)
+
+    # Log the reset response
+    logger.info(f"Reset Response: {reset_response}")
+
+    # Assert the expected outcome
+    assert reset_response["status"] in [
+        "success",
+        "error",
+    ], "The status should be either success or error"
+    if reset_response["status"] == "error":
+        assert (
+            "message" in reset_response
+        ), "The error response should contain a message"
+
+    # Print the response (optional)
+    print(reset_response)
+
+
+def test_user_access_management(setup, client):
+    # Generate a unique topic name
+    topic = "topic" + client.subject_openid[-12:]
+
+    # Ensure the topic is registered
+    client.register_topic(topic)
+
+    # Authorize another user to access the topic
+    user_id = "e2a8169b-feef-4d56-8eba-ab12747bee04"
+    grant_response = client.grant_user_access(topic, user_id)
+
+    # Log the grant response
+    logger.info(f"Grant Response: {grant_response}")
+
+    # # Assert the expected outcome for granting access
+    assert grant_response["status"] in [
+        "success",
+        "no-op",
+    ], "The status should be either success or no-op"
+    assert "message" in grant_response, "The response should contain a message"
+
+    # Print the grant response (optional)
+    print(grant_response)
+
+    # List users that have access to the topic
+    list_users_response = client.list_topic_users(topic)
+
+    # Log the list users response
+    logger.info(f"List Users Response: {list_users_response}")
+
+    # Assert the expected outcome for listing users
+    assert list_users_response["status"] == "success", "The status should be success"
+    assert "users" in list_users_response, "The response should contain users"
+    assert isinstance(list_users_response["users"], list), "Users should be a list"
+
+    # Print the list users response (optional)
+    print(list_users_response)
+
+    # Revoke user access to the topic
+    revoke_response = client.revoke_user_access(topic, user_id)
+
+    # Log the revoke response
+    logger.info(f"Revoke Response: {revoke_response}")
+
+    # Assert the expected outcome for revoking access
+    assert revoke_response["status"] in [
+        "success",
+        "no-op",
+    ], "The status should be either success or no-op"
+    assert "message" in revoke_response, "The response should contain a message"
+
+    # Print the revoke response (optional)
+    print(revoke_response)
 
 
 if __name__ == "__main__":
